c88b173d9c78dcea672ee4802a3ca6b6d0733175
festerico <72529292+festerico@users.noreply.github.com>
2020-10-07 19:50:31 -0300

    Add files via upload
    Ejercicio de UTN en c# programa para elevar a la 3era potencia.

---
+++ cubo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace cubo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            Console.WriteLine("ingrse un numero que desee elevar al cubo");
+            int numero =int.Parse(Console.ReadLine());
+            int resultado= numero*numero*numero;
+           Console.WriteLine ("el resultado es");
+
+    
+            Console.WriteLine (resultado);
+
+
+        }
+    }
+}
